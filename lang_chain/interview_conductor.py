2ebe509dbe9d02e789b23b55161573966ee483eb
Hiroto0706 <mirunina2366@icloud.com>
2025-03-09 11:55:43 +0900

    [delete] deleted unnecessary code

--- lang_chain/interview_conductor.py
+++ lang_chain/interview_conductor.py
@@ -1,12 +1,6 @@
-import operator
-from typing import Annotated, Any, Optional
-
-from dotenv import load_dotenv
 from langchain_core.output_parsers import StrOutputParser
 from langchain_core.prompts import ChatPromptTemplate
 from langchain_openai import ChatOpenAI
-from langgraph.graph import END, StateGraph
-from pydantic import BaseModel, Field
 
 from objects.interview import Interview, InterviewResult
 from objects.persona import Persona
